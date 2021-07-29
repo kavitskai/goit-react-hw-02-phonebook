03c1bded349122372d59fda9643abe68abf5d724
kavitskai <kavitskai@gmail.com>
2021-07-30 00:21:35 +0300

    HW 2

---
+++ src/App/App.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Main = styled.main`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+text-align: center;
+margin: 20px auto;
+padding: 20px;
+background-color: #fff;
+width: 900px;
+`;
+
+export const Section = styled.section`
+  `;
